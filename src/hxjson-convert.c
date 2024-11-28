b66122f93f6f877ac6a33a34069e1e24ef78a693
Alp Yilmaz <Hexa3333@protonmail.com>
2024-11-28 17:23:13 +0300

    Move conversion functions

--- src/hxjson-convert.c
+++ src/hxjson-convert.c
@@ -2,15 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+static void Convert_String(char* val)
+{
+  int len = strlen(val);
+  memmove(val, val+1, len-2);
+  val[len-2] = 0;
+}
+
+static hxjsonInt Convert_Integer(char* val)
+{
+  return strtoq(val, NULL, 0);
+}
+
+static hxjsonFloat Convert_Float(char* val)
+{
+  return strtof(val, NULL);
+}
+
+static hxjsonBool Convert_Boolean(char* val)
+{
+  bool ret;
+
+  if (strcmp(val, "true") == 0)
+    ret = true;
+  else
+    ret = false;
+
+  return ret;
+}
+
 char* hxjsonGetString(char* key, struct hxjson* json)
 {
   char* val = hxjsonGet(key, json);
   if (!val) return 0;
 
-  // String conversion logic
-  int len = strlen(val);
-  memmove(val, val+1, len-2);
-  val[len-2] = 0;
+  Convert_String(val);
   return val;
 }
 
@@ -20,7 +46,7 @@ hxjsonInt hxjsonGetInt(char* key, struct hxjson* json)
   if (!valStr) return 0;
 
   // Integer conversion logic
-  hxjsonInt ret = strtoq(valStr, NULL, 0);
+  hxjsonInt ret = Convert_Integer(valStr);
 
   free(valStr);
   return ret;
@@ -32,7 +58,7 @@ hxjsonFloat hxjsonGetFloat(char* key, struct hxjson* json)
   if (!valStr) return 0;
 
   // Float conversion logic
-  hxjsonFloat ret = strtof(valStr, NULL);
+  hxjsonFloat ret = Convert_Float(valStr);
 
   free(valStr);
   return ret;
@@ -44,11 +70,7 @@ hxjsonBool hxjsonGetBool(char* key, struct hxjson* json)
   if (!valStr) return false;
 
   // Bool conversion logic
-  hxjsonBool ret;
-  if (strcmp(valStr, "true") == 0)
-    ret = true;
-  else
-    ret = false;
+  hxjsonBool ret = Convert_Boolean(valStr);
   
   free(valStr);
   return ret;
